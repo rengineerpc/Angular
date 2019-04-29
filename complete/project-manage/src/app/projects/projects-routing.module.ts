8a1067511466f55a257424e70ffc25232395a2f7
rengineerpc <rengineerpc@gmail.com>
2019-04-29 09:36:31 -0500

    Add files via upload

---
+++ complete/project-manage/src/app/projects/projects-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {ProjectsContainerComponent} from './projects-container/projects-container.component';
+import {ProjectDetailContainerComponent} from './project-detail-container/project-detail-container.component';
+
+const routes: Routes = [
+  {path: 'projects', component: ProjectsContainerComponent},
+  {path: 'projects/:id', component: ProjectDetailContainerComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProjectsRoutingModule { }
